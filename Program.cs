d64950b85ed76fb29a6d07548d4d637368be2caa
Morteza Tajerii <mortezatajerii@gmail.com>
2023-02-08 22:04:36 +0330

    Add Conditions
    Condition for realize; how a BMI became underweight, normal, overweight, obese

--- Program.cs
+++ Program.cs
@@ -7,11 +7,11 @@ namespace BMICalculator
         static void Main(string[] args)
         {
             Console.Write("Please enter weight in KG: ");
-            double weight = Convert.ToDouble(Console.ReadLine()); 
+            double weight = Convert.ToDouble(Console.ReadLine());
 
             Console.Write("Please enter hight in CM: ");
             double hight = Convert.ToDouble(Console.ReadLine());
-            
+
             hight = hight / 100;
 
             // * Declare weight in Kg and hight in CM
@@ -20,9 +20,28 @@ namespace BMICalculator
             double BMI = weight / (hight * hight);
             BMI = Math.Round(BMI, 2);
 
-            // * Math.Round Method limits decimal digits
+            ////  Math.Round Method limits decimal digits
+            //// Console.WriteLine($"Your BMI is {BMI}");
 
-            Console.WriteLine($"Your BMI is {BMI}");
+            if (BMI < 18.5)
+            {
+                Console.WriteLine($"Your BMI is equal to {BMI}, and you are underweight.");
+            }
+            
+            else if (BMI >= 18.5 & BMI < 25)
+            {
+                Console.WriteLine($"Your BMI is equal to {BMI}, and you are normal.");
+            }
+
+            else if (BMI >= 25 & BMI < 30)
+            {
+                Console.WriteLine($"Your BMI is equal to {BMI}, and you are overweight.");
+            }
+
+            else if (BMI >= 30)
+            {
+                Console.WriteLine($"Your BMI is equal to {BMI}, and you are obese.");
+            }
         }
     }
 }
